e8bb1cd9066632cd7092c55853c666a9de6f0b71
Misha Ship <monteship@gmail.com>
2022-12-10 06:00:37 +0200

    fix

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
 import sched
+
+import lxml
 import requests
 import pandas as pd
 from notifypy import Notify
-
+from bs4 import BeautifulSoup
 
 s = sched.scheduler(time.time, time.sleep)
 
@@ -18,32 +20,28 @@ class PowerOffLoe:
         self.address = city, street, otg
         self.result = []
         self.tables = None
-        self.__run()
-
-    def __run(self):
-        self.__scrap_page()
         self.__get_accident()
+        self.__get_schedule()
         self.__send_notify()
 
-    def __scrap_page(self):
+    def __get_accident(self):
         try:
             url = "https://poweroff.loe.lviv.ua/search_off?csrfmiddlewaretoken=" \
                   "QRUjXkiT8HWXOxqCn5y8mezrCJQuWaLzJMwKaykvgNYALnaUClTiBB3usHb0O" \
                   "rlF&city={}&street={}&otg={}&q=%D0%9F%D0%BE%D1%88%D1%83%D0%BA".format(*self.address, {})
             page = requests.get(url)
-            self.tables = pd.read_html(page.text)
+            tables = pd.read_html(page.text)
+            table = tables[2]
+            self.result = "В {2}.\n{5}, {6}.\nЗ {7} \nПо {8}.".format(*table.values[0])
+        except IndexError:
+            self.result = f"В {self.address[0].capitalize()} аварійних відключень непередбачено!"
         except requests.exceptions.ConnectTimeout:
             self.result = "Відсутній інтернет"
 
-    def __get_accident(self):
-        try:
-            table = self.tables[2]
-            print(table.values)
-            self.result = "В {2}.\n{5}, {6}.\nЗ {7} \nПо {8}.".format(*table.values[0])
-        except AttributeError:
-            self.result = f"В {self.address[0].capitalize()} аварійних відключень непередбачено!"
-        except IndexError:
-            self.result = f"В {self.address[0].capitalize()} аварійних відключень непередбачено!"
+    def __get_schedule(self):  # ToDo
+        url = 'https://poweroff.loe.lviv.ua/gav_city3'
+        page = requests.get(url)
+        send_post = requests.post(url, cookies=page.cookies)
 
     def __send_notify(self):
         notification = Notify()
@@ -53,7 +51,7 @@ class PowerOffLoe:
 
 
 def start(sc):
-    city = ''
+    city = 'Сіде'
     street = ''
     otg = ''
     PowerOffLoe(city, street, otg)
